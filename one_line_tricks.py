66fc6596ef0b83a0695f8d993956008fbaffa905
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2019-03-05 10:59:42 +0300

    Tek satır ile if ve for kullanımı eklendi

---
+++ one_line_tricks.py
@@ -0,0 +1,21 @@
+# tek satirda if ile deger atama
+# (atanacak deger) if (kosul) else (kosul saglanmazsa atanacak deger)
+
+deger = 'Python' if 10<11 else 'Ruby'
+print(deger)
+# Python
+
+# tek satirda for ile list olusturma
+# [ (listeye eklenecek degisken) for (for ile gelen data) in (for ile dolaşılan data))
+
+data = [1, 2, 3, 4, 5]
+array = [ x+2 for x in data ]
+print(array)
+# [3, 4, 5, 6, 7]
+
+# eger istenirse if ile de kullanılabilir
+
+array = [ x+2 for x in data if x<3 ]
+print(array)
+# [3, 4]
+
